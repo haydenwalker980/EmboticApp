ce61ca606be4dd6db2a4373aac16719e4fbb0883
xdlivecat <xdliverblx@hotmail.com>
2024-03-10 13:11:08 +0100

    Remove activities from AndroidManifest.xml

--- app/src/main/java/com/embotic/nodes/MainActivity.kt
+++ app/src/main/java/com/embotic/nodes/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.embotic.nodes
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 private const val TAG = "MainActivity"
 
@@ -8,5 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
     }
 }
