19a6d29604fb890cc859c9643431c9b1ffa69c37
xdlivecat <xdliverblx@hotmail.com>
2024-04-14 18:02:58 +0200

    Added about menu

--- app/src/main/java/com/embotic/nodes/AboutActivity.kt
+++ app/src/main/java/com/embotic/nodes/AboutActivity.kt
@@ -1,11 +1,19 @@
 package com.embotic.nodes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.about_layout)
+
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
-}
+    }
