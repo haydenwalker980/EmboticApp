26f413b7ce800e9300fadd7a546240bff9538dd4
xdlivecat <xdliverblx@hotmail.com>
2024-05-10 17:14:58 +0200

    kjhxfskjhasdf

--- app/src/main/java/com/embotic/nodes/MainActivity.kt
+++ app/src/main/java/com/embotic/nodes/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -13,6 +14,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -25,6 +27,7 @@ import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
+import com.embotic.nodes.databinding.MainActivityBinding
 import java.util.concurrent.atomic.AtomicBoolean
 
 private const val TAG = "MainActivity"
@@ -32,19 +35,82 @@ private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     lateinit var webView: WebView
+    private val isMobileAdsInitializeCalled = AtomicBoolean(false)
+    private val initialLayoutComplete = AtomicBoolean(false)
+    private lateinit var binding: MainActivityBinding
+    private lateinit var adView: AdView
+    private lateinit var googleMobileAdsConsentManager: GoogleMobileAdsConsentManager
     private val  appUpdateManager: AppUpdateManager? = AppUpdateManagerFactory.create(this)
 
     private val dialog: AlertDialog? = null
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = binding.adViewContainer.width.toFloat()
+            if (adWidthPixels == 0f) {
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+            }
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
         if (resources.getBoolean(R.bool.isTablet)) {
             // This is a tablet, so use the tablet layout
             setContentView(R.layout.main_activity_tablet)
         } else {
             // This is not a tablet, use the default layout
-            setContentView(R.layout.main_activity)
+            setContentView(binding.root)
         }
+
+        adView = AdView(this)
+        binding.adViewContainer.addView(adView)
+
+        googleMobileAdsConsentManager = GoogleMobileAdsConsentManager.getInstance(applicationContext)
+        googleMobileAdsConsentManager.gatherConsent(this) { error ->
+            if (error != null) {
+                // Consent not obtained in current session.
+                Log.d(TAG, "${error.errorCode}: ${error.message}")
+            }
+
+            if (googleMobileAdsConsentManager.canRequestAds) {
+                initializeMobileAdsSdk()
+            }
+
+            if (googleMobileAdsConsentManager.isPrivacyOptionsRequired) {
+                // Regenerate the options menu to include a privacy setting.
+                invalidateOptionsMenu()
+            }
+        }
+
+        // This sample attempts to load ads using consent obtained in the previous session.
+        if (googleMobileAdsConsentManager.canRequestAds) {
+            initializeMobileAdsSdk()
+        }
+
+        // Since we're loading the banner based on the adContainerView size, we need to wait until this
+        // view is laid out before we can get the width.
+        binding.adViewContainer.viewTreeObserver.addOnGlobalLayoutListener {
+            if (!initialLayoutComplete.getAndSet(true) && googleMobileAdsConsentManager.canRequestAds) {
+                loadBanner()
+            }
+        }
+
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder().setTestDeviceIds(listOf("ABCDEF012345")).build()
+        )
     
 
         // Check if the device is a tablet and set screen orientation accordingly
@@ -79,6 +145,24 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("https://dash.embotic.xyz/")
     }
 
+    /** Called when leaving the activity. */
+    public override fun onPause() {
+        adView.pause()
+        super.onPause()
+    }
+
+    /** Called when returning to the activity. */
+    public override fun onResume() {
+        super.onResume()
+        adView.resume()
+    }
+
+    /** Called before the activity is destroyed. */
+    public override fun onDestroy() {
+        adView.destroy()
+        super.onDestroy()
+    }
+
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -87,13 +171,51 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val menuItemView = findViewById<View>(item.itemId)
         return when (item.itemId) {
             R.id.action_about -> {
                 val intent = Intent(this, AboutActivity::class.java)
                 startActivity(intent)
                 true
             }
-            else -> super.onOptionsItemSelected(item)
+
+            R.id.privacy_settings -> {
+                // Handle changes to user consent.
+                googleMobileAdsConsentManager.showPrivacyOptionsForm(this@MainActivity) { formError ->
+                    if (formError != null) {
+                        Toast.makeText(this@MainActivity, formError.message, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                true
+            }
+            else -> false
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun loadBanner() {
+        // This is an ad unit ID for a test ad. Replace with your own banner ad unit ID.
+        adView.adUnitId = "ca-app-pub-3940256099942544/9214589741"
+        adView.setAdSize(adSize)
+
+        // Create an ad request.
+        val adRequest = AdRequest.Builder().build()
+
+        // Start loading the ad in the background.
+        adView.loadAd(adRequest)
+    }
+
+    private fun initializeMobileAdsSdk() {
+        if (isMobileAdsInitializeCalled.getAndSet(true)) {
+            return
+        }
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this) {}
+
+        // Load an ad.
+        if (initialLayoutComplete.get()) {
+            loadBanner()
         }
     }
 }
