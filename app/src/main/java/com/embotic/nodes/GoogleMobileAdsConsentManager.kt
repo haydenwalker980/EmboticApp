1bf8b73a725762e0553b3942604bd75825f3722e
xdlivecat <xdliverblx@hotmail.com>
2024-05-10 19:03:20 +0200

    I forgot the ad unit ID

--- app/src/main/java/com/embotic/nodes/GoogleMobileAdsConsentManager.kt
+++ app/src/main/java/com/embotic/nodes/GoogleMobileAdsConsentManager.kt
@@ -11,11 +11,6 @@ import com.google.android.ump.ConsentRequestParameters
 import com.google.android.ump.FormError
 import com.google.android.ump.UserMessagingPlatform
 
-/**
- * The Google Mobile Ads SDK provides the User Messaging Platform (Google's IAB Certified consent
- * management platform) as one solution to capture consent for users in GDPR impacted countries.
- * This is an example and you can choose another consent management platform to capture consent.
- */
 class GoogleMobileAdsConsentManager private constructor(context: Context) {
     private val consentInformation: ConsentInformation =
         UserMessagingPlatform.getConsentInformation(context)
